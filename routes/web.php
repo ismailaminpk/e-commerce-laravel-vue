a9e83bb4b3405c4e8b0244ac7140d6ac5604afeb
Ismail A. <ismailamin.pk@gmail.com>
2024-11-09 22:44:45 +0500

    create admin and admin role
    roles and user roles migrations, default admin user and auth controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('admin.index');
 });
+
+Route::get('/admin-create', [AuthController::class, 'createUser'])->name('admin.create');
